c6187ec57425d89e2d30b95c28a66cfa3313e233
Gustavo de Carli <gdecarli4@gmail.com>
2025-10-01 19:15:18 -0300

    Adiciona JSDoc

--- backend/src/services/eventsService.js
+++ backend/src/services/eventsService.js
@@ -1,6 +1,16 @@
 const EventModel = require('../models/eventModel')
 
 class EventsService {
+  /**
+  * Serviço de Eventos: regras de negócio relacionadas a eventos.
+  * @module services/EventsService
+  */
+
+  /**
+  * Valida o nome do usuário.
+  * @returns {Array} Array de eventos
+  * @throws {Error} Se nenhum evento encontrado
+  */
   static async listagem() {
     const listagem = await EventModel.listagem()
     if (listagem.length == 0) {
