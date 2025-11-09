d9f4f4b01f0fd0e37d57ce80f4b74e46f35adf39
Gustavo-DeCarli <gdecarli4@gmail.com>
2025-11-09 17:50:39 -0300

    Ajusta import

--- backend/src/tests/app.test.js
+++ backend/src/tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const { app } = require('../app');
 
 describe('Login usuário', () => {
     test('Login com sucesso', async () => {
